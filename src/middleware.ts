1a822e092d33a0c5252c9d65fe0077851df6dacf
Vu Tung Lam <vutunglaminfo@gmail.com>
2024-02-09 10:10:42 +0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,8 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     req.headers.set('X-Lng', req.geo?.country || "unknown to Vercel Geolocationing");
+    return NextResponse.next({
+        headers: req.headers,
+    });
 }
