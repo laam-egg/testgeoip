6079a7f5b023fe64910ef3083e72871cda200c27
Vu Tung Lam <vutunglaminfo@gmail.com>
2024-07-29 22:51:55 +0700

    feat: use new Vercel headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,26 @@ export default async function Home() {
   const country = (await getGeoipInfo(ip)).country;
   const countryByVercel = headers().get('x-lng') || "unknown to Vercel";
 
+  // https://vercel.com/docs/edge-network/headers#x-vercel-ip-country
+  const countryInXVercelIpCountry = headers().get('x-vercel-ip-country');
+
   return (
     <main>
       <div>
         <p>
           IP address: {ip} ; country: {country} ; country detected by Vercel: {countryByVercel}
+        </p>
+        <p>
+          Country detected in <code>X-Vercel-IP-Country</code> header: {headers().get('x-vercel-ip-country')}
+        </p>
+        <p>
+          Detected Continent: {headers().get('x-vercel-ip-country-region')}
+        </p>
+        <p>
+          Detected City: {headers().get('x-vercel-ip-city')}
+        </p>
+        <p>
+          Detected Timezone: {headers().get('x-vercel-ip-timezone')}
         </p>
         <IpAddressLord />
       </div>
